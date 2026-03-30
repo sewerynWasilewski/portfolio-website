19d1d05610be5f5cda8e8ae53b186c7092f5ae82
Seweryn Wasilewski <seweryn.wasilewski@gmail.com>
2026-03-30 23:15:19 +0200

    feat: add routing for home, blog and projects pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ProjectDetailsComponent } from './features/projects/project-details/project-details.component';
+import { BlogComponent } from './features/blog/blog.component';
+import { BlogDetailsComponent } from './features/blog/blog-details/blog-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
+    path: 'projects/:project_id',
+    component: ProjectDetailsComponent,
+  },
+  {
+    path: 'blogs',
+    component: BlogComponent,
+  },
+  {
+    path: 'blogs/:blog_id',
+    component: BlogDetailsComponent,
+  },
+];
